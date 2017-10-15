9aa7cd6aec7f5abe3ae617e14af2815951082093
Luke Dupin <lukedupin@gmail.com>
2017-10-15 15:05:52 -0700

    System to handle output tracks started

--- handler_auto_class.py
+++ handler_auto_class.py
@@ -1,8 +1,42 @@
 from lex_rules import Token, ScannerToken
 from symbol_table import Symbol, SubSymbol, Node
+from tracks import Track, TrackType
+
+class ModAutoClass:
+    def __init__(self, depth):
+        self.depth = depth
+
 
 def handleAutoClass( node, start, end ):
     tokens = [ ScannerToken( Token.CLASS, "class", node.children[0].line),
                node.children[0] ]
 
     return start + tokens + end
+
+def tracksAutoClass( tokens, matches, depth, tracks ):
+    # Check if we are starting an auto class
+    if Symbol.AUTO_CLASS in matches:
+        tracks = Track.insert_track( tracks,
+                                     TrackType.SOURCE,
+                                     modifiers={ Symbol.AUTO_CLASS, ModAutoClass(depth)} )
+        tracks = Track.insert_track( tracks,
+                                     TrackType.HEADER,
+                                     modifiers={ Symbol.AUTO_CLASS, ModAutoClass(depth)} )
+
+    # Get my source and header index, if we have any
+    source = Track.find_track( tracks, TrackType.SOURCE, Symbol.AUTO_CLASS )
+    header = Track.find_track( tracks, TrackType.HEADER, Symbol.AUTO_CLASS )
+
+    # Is there no mode at all? quit
+    if source is None or header is None:
+        return tracks
+
+    tracks[source].lines.append( tokens )
+    tracks[header].lines.append( tokens )
+
+    return tracks
+
+
+
+
+
--- main.py
+++ main.py
@@ -7,20 +7,22 @@ from parser import parser
 from lex_rules import build_patterns
 from symbol_table import build_productions, build_subproductions, Symbol
 
-from handler_auto_class import handleAutoClass
+from handler_auto_class import handleAutoClass, tracksAutoClass
 from handler_template_class import handleTemplateClass
+from tracks import write_track, Track
 
 def main( argv ):
     patterns = build_patterns()
     productions = build_productions()
     sub_productions = build_subproductions()
 
-    tokens = []
-
     argv.pop(0)
     for filename in argv:
+        depth = 0
         line_count = 0
-        lines = []
+        tracks = []
+        tokens = []
+        matches = []
 
         # Read in the file contents
         for line in open( filename ).readlines():
@@ -38,18 +40,29 @@ def main( argv ):
                 for production in productions:
                     node, start_idx, end_idx = parser( tokens, production.production, production.symbols, production.build, sub_productions )
                     if node is not None:
+                        matches.append( node.production )
                         if node.production == Symbol.AUTO_CLASS:
                             tokens = handleAutoClass( node, tokens[0:start_idx], tokens[end_idx:] )
 
                         elif node.production == Symbol.TEMPLATE_CLASS:
                             tokens = handleTemplateClass( node, tokens[0:start_idx], tokens[end_idx:] )
 
-                #Store my updated line and continue
-                lines.append( TokenExporter( tokens, exporters ) )
+                # Reduce the depth
+                if token.token == '}':
+                    depth -= 1
+
+                # We've got the raw tokens, now pass that completed set of tokens into the track creates to build out tracks
+                tracks = tracksAutoClass( tokens, matches, depth, tracks )
+                #tracks = tracksTemplateclass( tokens, matches, depth, tracks )
                 tokens.clear()
 
+                # Increase the depth
+                if token.token == '{':
+                    depth += 1
+
         # Dump all the now collected tokens into the output file
-        exporter.store()
+        for track in tracks:
+            write_track( track )
 
 
 
